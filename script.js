74b76e85e033d0fe231bac7aacdddf672ce387be
poojitha <poojithabocha63@gmail.com>
2026-04-21 19:05:59 +0530

    added changes in script.js

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
-const add = () => {
+const add = (event) => {
   event.preventDefault();
 
-  let val1 = Number(document.getElementById("value1").value);
-  let val2 = Number(document.getElementById("value2").value);
+  let val1 = Number(document.getElementById("num1").value);
+  let val2 = Number(document.getElementById("num2").value);
 
   let display = document.getElementById("display");
 
   display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 + val2}`;
 };
-const sub = () => {
+
+const sub = (event) => {
   event.preventDefault();
 
-  let val1 = Number(document.getElementById("value1").value);
-  let val2 = Number(document.getElementById("value2").value);
+  let val1 = Number(document.getElementById("num1").value);
+  let val2 = Number(document.getElementById("num2").value);
 
   let display = document.getElementById("display");
 
-  display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 - val2}`;
+  display.innerHTML = `The difference of ${val1} and ${val2} is ${val1 - val2}`;
 };
 
-const multi = () => {
+const multi = (event) => {
   event.preventDefault();
 
-  let val1 = Number(document.getElementById("value1").value);
-  let val2 = Number(document.getElementById("value2").value);
+  let val1 = Number(document.getElementById("num1").value);
+  let val2 = Number(document.getElementById("num2").value);
 
   let display = document.getElementById("display");
 
-  display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 * val2}`;
+  display.innerHTML = `The product of ${val1} and ${val2} is ${val1 * val2}`;
 };
 
-const divi = () => {
+const divi = (event) => {
   event.preventDefault();
 
-  let val1 = Number(document.getElementById("value1").value);
-  let val2 = Number(document.getElementById("value2").value);
+  let val1 = Number(document.getElementById("num1").value);
+  let val2 = Number(document.getElementById("num2").value);
 
   let display = document.getElementById("display");
 
-  display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 / val2}`;
+  display.innerHTML = `The division of ${val1} and ${val2} is ${val1 / val2}`;
 };
 
-const modulus = () => {
+const modulus = (event) => {
   event.preventDefault();
 
-  let val1 = Number(document.getElementById("value1").value);
-  let val2 = Number(document.getElementById("value2").value);
+  let val1 = Number(document.getElementById("num1").value);
+  let val2 = Number(document.getElementById("num2").value);
 
   let display = document.getElementById("display");
 
-  display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 % val2}`;
+  display.innerHTML = `The remainder of ${val1} and ${val2} is ${val1 % val2}`;
 };
 
-const power = () => {
+const power = (event) => {
   event.preventDefault();
 
-  let val1 = Number(document.getElementById("value1").value);
-  let val2 = Number(document.getElementById("value2").value);
+  let val1 = Number(document.getElementById("num1").value);
+  let val2 = Number(document.getElementById("num2").value);
 
   let display = document.getElementById("display");
 
-  display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 ** val2}`;
+  display.innerHTML = `${val1} power ${val2} is ${val1 ** val2}`;
 };
